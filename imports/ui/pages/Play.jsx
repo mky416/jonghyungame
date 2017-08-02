49e4f55e94819fc92035c72e38f89d9dbf3b6166
mky416 <mky416@gmail.com>
2017-08-02 09:12:11 +0000

    quiz

--- imports/ui/pages/Play.jsx
+++ imports/ui/pages/Play.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
+const styles ={
+    white: {
+        maxWidth: '600px', position:'absolute',
+    },
+    disc: {
+        maxWidth: '200px', position: 'relative', left: '200px', top: '25px'
+    }
+}
 
 export default class Play extends React.Component {
     render() {
         return (
             <div>
-                <h1>Play</h1>
+                <img src="/images/play22.png" style={styles.white} />
+                <img src="/images/compact-disc.png" style={styles.disc}/>               
             </div>
         );
 
